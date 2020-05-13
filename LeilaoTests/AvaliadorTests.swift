aefe54a1eaf955223b011d87362576c4b862bc09
Marco Aurélio Sanches Gonçalves <marco182.2@gmail.com>
2020-05-13 13:59:11 -0300

    [AULA-02] Escrevendo testes de unidade

--- LeilaoTests/AvaliadorTests.swift
+++ LeilaoTests/AvaliadorTests.swift
@@ -38,4 +38,16 @@ class AvaliadorTests: XCTestCase {
         XCTAssertEqual(250.0, leiloeiro.menorLance())
         XCTAssertEqual(400.0, leiloeiro.maiorLance())
     }
+    
+    func testDeveEntenderLeilaoComApenasUmLance() {
+        let joao = Usuario(nome: "Joao")
+        let leilao = Leilao(descricao: "Playstation 4")
+        leilao.propoe(lance: Lance(joao, 1000.0))
+        
+        let leiloeiro = Avaliador()
+        leiloeiro.avalia(leilao: leilao)
+        
+        XCTAssertEqual(1000.0, leiloeiro.menorLance())
+        XCTAssertEqual(1000.0, leiloeiro.maiorLance())
+    }
 }
