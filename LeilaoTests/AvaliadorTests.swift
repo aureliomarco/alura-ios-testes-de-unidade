54610fa80ba475f2bcb0377750107000b8233e52
Marco Aurélio Sanches Gonçalves <marco182.2@gmail.com>
2020-05-25 22:41:00 -0300

    [AULA-05] Qualidade dos testes

--- LeilaoTests/AvaliadorTests.swift
+++ LeilaoTests/AvaliadorTests.swift
@@ -10,9 +10,18 @@ import XCTest
 @testable import Leilao
 
 class AvaliadorTests: XCTestCase {
+    
+    var leiloeiro: Avaliador?
+    private var joao: Usuario?
+    private var maria: Usuario?
+    private var jose: Usuario?
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        joao = Usuario(nome: "Joao")
+        jose = Usuario(nome: "Jose")
+        maria = Usuario(nome: "Maria")
+        leiloeiro = Avaliador()
     }
 
     override func tearDown() {
@@ -20,55 +29,43 @@ class AvaliadorTests: XCTestCase {
     }
     
     func testDeveEntenderLanchesEmOrdemCrescente() {
-        // Cenário
-        let joao = Usuario(nome: "Joao")
-        let jose = Usuario(nome: "Jose")
-        let maria = Usuario(nome: "Maria")
-        
         let leilao = Leilao(descricao: "Playstation 4")
-        leilao.propoe(lance: Lance(maria, 250.0))
-        leilao.propoe(lance: Lance(joao, 300.0))
-        leilao.propoe(lance: Lance(jose, 400.0))
+        leilao.propoe(lance: Lance(maria ?? Usuario(nome: ""), 250.0))
+        leilao.propoe(lance: Lance(joao ?? Usuario(nome: ""), 300.0))
+        leilao.propoe(lance: Lance(jose ?? Usuario(nome: ""), 400.0))
         
         // Ação
-        let leiloeiro = Avaliador()
-        leiloeiro.avalia(leilao: leilao)
+        leiloeiro?.avalia(leilao: leilao)
         
         // Validação
-        XCTAssertEqual(250.0, leiloeiro.menorLance())
-        XCTAssertEqual(400.0, leiloeiro.maiorLance())
+        XCTAssertEqual(250.0, leiloeiro?.menorLance())
+        XCTAssertEqual(400.0, leiloeiro?.maiorLance())
     }
     
     func testDeveEntenderLeilaoComApenasUmLance() {
-        let joao = Usuario(nome: "Joao")
         let leilao = Leilao(descricao: "Playstation 4")
-        leilao.propoe(lance: Lance(joao, 1000.0))
+        leilao.propoe(lance: Lance(joao ?? Usuario(nome: ""), 1000.0))
         
-        let leiloeiro = Avaliador()
-        leiloeiro.avalia(leilao: leilao)
+        leiloeiro?.avalia(leilao: leilao)
         
-        XCTAssertEqual(1000.0, leiloeiro.menorLance())
-        XCTAssertEqual(1000.0, leiloeiro.maiorLance())
+        XCTAssertEqual(1000.0, leiloeiro?.menorLance())
+        XCTAssertEqual(1000.0, leiloeiro?.maiorLance())
     }
     
     func testDeveEncontrarOsTresMaioresLances() {
-        let joao = Usuario(nome: "Joao")
-        let maria = Usuario(nome: "Maria")
-        
         let leilao = Leilao(descricao: "Playstation 4")
-        leilao.propoe(lance: Lance(joao, 300.0))
-        leilao.propoe(lance: Lance(maria, 400.0))
-        leilao.propoe(lance: Lance(joao, 500.0))
-        leilao.propoe(lance: Lance(maria, 600.0))
+        leilao.propoe(lance: Lance(joao ?? Usuario(nome: ""), 300.0))
+        leilao.propoe(lance: Lance(maria ?? Usuario(nome: ""), 400.0))
+        leilao.propoe(lance: Lance(joao ?? Usuario(nome: ""), 500.0))
+        leilao.propoe(lance: Lance(maria ?? Usuario(nome: ""), 600.0))
         
-        let leiloeiro = Avaliador()
-        leiloeiro.avalia(leilao: leilao)
+        leiloeiro?.avalia(leilao: leilao)
         
-        let listaLances = leiloeiro.tresMaiores()
+        let listaLances = leiloeiro?.tresMaiores()
         
-        XCTAssertEqual(3, listaLances.count)
-        XCTAssertEqual(600.0, listaLances[0].valor)
-        XCTAssertEqual(500.0, listaLances[1].valor)
-        XCTAssertEqual(400.0, listaLances[2].valor)
+        XCTAssertEqual(3, listaLances?.count)
+        XCTAssertEqual(600.0, listaLances?[0].valor)
+        XCTAssertEqual(500.0, listaLances?[1].valor)
+        XCTAssertEqual(400.0, listaLances?[2].valor)
     }
 }
